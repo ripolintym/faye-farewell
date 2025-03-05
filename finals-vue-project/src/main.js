6cf17295f8a4f9a4661bb249d12a91d0aae6143b
apcpmsumilhig <pmsumilhig@student.apc.edu.ph>
2025-03-05 09:38:03 +0000

    Last Check

--- finals-vue-project/src/main.js
+++ finals-vue-project/src/main.js
@@ -4,4 +4,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import Home from './components/home.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.component('Home', Home)
+
+app.mount('#app')
