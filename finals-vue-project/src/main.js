8aab164a4ab95d0167575e4d833153b1ef580661
apcpmsumilhig <pmsumilhig@student.apc.edu.ph>
2025-03-05 09:40:30 +0000

    tired

--- finals-vue-project/src/main.js
+++ finals-vue-project/src/main.js
@@ -2,7 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import Home from './components/home.vue'
+import Home from './components/Home.vue'
 
 const app = createApp(App)
 app.component('Home', Home)
