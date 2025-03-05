6d48951b41c61996b7c27185d0b9e3cb92de4984
apcpmsumilhig <pmsumilhig@student.apc.edu.ph>
2025-03-05 09:27:35 +0000

    Input Check

---
+++ finals-vue-project/src/components/home.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <main>
+      <section>
+        <div>
+          <!-- About Me -->
+          <div>
+            <h3>About Me</h3>
+            <p>
+              I am a 2nd Year student currently in office as an officer for two organizations. Also inclined with analytics, statistics, as well as data science and similar topics.
+            </p>
+          </div>
+
+          <div>
+            <div>
+              <h3>Education</h3>
+              studied two different science high schools, one for Junior High School at 
+              <a href="https://www.facebook.com/profile.php?id=100083844947925" target="_blank">Senator Renato Compañero Cayetano Memorial Science & Technology High School</a> 
+              and another one located at a different city for Senior High School at 
+              <a href="https://www.facebook.com/pcshs.edu" target="_blank">Pasig City Science High School</a>. 
+              With Honors as a grade 12 student as well as a First Year College Student.
+              <br />
+              <b>Achievements</b>
+              <ul>
+                <li>Was the Memberships head for Gaming Genesis</li>
+                <li>Currently the Logistics Head for APC Microsoft Community</li>
+                <li>Current President for Gaming Genesis</li>
+              </ul>
+            </div>
+          </div>
+
+          <div>
+            <div>
+              <h3>Course</h3>
+              Currently studying as a Bachelor of Science in Computer Science specializing in Software Systems.
+            </div>
+          </div>
+
+          <div>
+            <div>
+              <h3>IT Experience</h3>
+              Had a 
+              <a href="https://docs.google.com/document/d/1IbS4nP_PUzZyqjC5f1uV26DvfdhpoYD7kBNJlVIQJgo/edit?usp=sharing" target="_blank">Mobile Development Project</a> 
+              that I developed alone for a Group Research Project using Java that works similarly to an Air Tag but has ways of communication to send information for different scenarios such as natural disasters as well as man-made scenarios.
+            </div>
+          </div>
+
+          <div>
+            <div>
+              <h3>Hobbies & Interests</h3>
+              As a person who doesn't really have anything to do in life, I commonly find myself doing these activities apart from looking at landscapes or "views" like the city lights in the evening.
+              <ul>
+                <li>Reading
+                  <ul>
+                    <li><a href="https://comick.io" target="_blank">Manga/Manhwa</a></li>
+                    <li><a href="https://comick.io" target="_blank">Webtoons</a></li>
+                  </ul>
+                </li>
+                <li>Sleeping</li>
+                <li>Cooking</li>
+                <li>Photography</li>
+                <li>Travelling</li>
+              </ul>
+            </div>
+          </div>
+
+          <div>
+            <div>
+              <h3>Goals In Life / Dream</h3>
+              to be so financially stable that I can spend on a day the average spending power of a person on a year.
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  </div>
+</template>
+
+<script></script>
+
+<style></style>
--- finals-vue-project/src/main.js
+++ finals-vue-project/src/main.js
@@ -2,5 +2,6 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import Home from './components/home.vue'
 
 createApp(App).mount('#app')
