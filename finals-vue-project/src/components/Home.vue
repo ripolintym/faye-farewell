975f8c406c16a37f62d54269562b494c5276925f
apcpmsumilhig <pmsumilhig@student.apc.edu.ph>
2025-03-05 10:34:38 +0000

    check both

--- finals-vue-project/src/components/Home.vue
+++ finals-vue-project/src/components/Home.vue
@@ -1,98 +1,128 @@
 <template>
-  <div>
-    <header>
-      <div>
-        <div>
-          <h6>normally.pol</h6>
-        </div>
-        <div class="socials-holder">
-          <div>
-            <p>About</p>
+
+<!-- Header Tab -->
+  <header class = "header">
+    <div class = "header-holder">
+      <div class = "header-text">
+        <h1>Personal Profile Web page </h1>
+      </div>
+      <div class = "header-tabs">
+        <div class= "tab"><h3> <a href = "">About Me </a></h3></div>
+        <div class= "tab"><h3> <a href = "gallery">Picture Gallery</a></h3></div>
+        <div class= "tab"><h3><a href = "">Contact Me!</a></h3></div>
+  
+      </div>
+        
+      
+    </div>
+    </header>
+
+<!-- Profile Section -->
+    <section class = profile>
+      <div class = "profile-holder">
+        <div class = "profile-container">
+          <div class = "picture">
+            <img src = "css/assets/profile.jpeg" id = "profile-picture" >
           </div>
-          <div>
-            <p>Experience</p>
+          <div class = "intro">
+            <h3>Hi I'm Paul Brian Sumilhig!</h3>
           </div>
-          <div>
-            <p>Socials</p>
+          <div class = "profile-description">
+            <h5>And you can find me here!</h5>
+          </div>
+          <div class = "socials-container">
+            <div class = "socials-tab">
+              <a href = "https://www.linkedin.com/in/paul-brian-sumilhig-531643320/" target="_blank">  LinkedIn  </a> 
+            </div>
+            <div class = "socials-tab">
+              <a href = "https://github.com/apcpmsumilhig" target="_blank" > GitHub </a> 
+            </div>
+            <div class = "socials-tab">
+              <a href = "https://www.facebook.com/normally.pol" target="_blank">  Facebook  </a> 
+            </div>
           </div>
         </div>
       </div>
-    </header>
+    </section>
 
+  <!-- Main Content -->
+  <div class="container">
+    
     <main>
-      <section>
-        <div>
-          <!-- About Me -->
-          <div>
-            <h3>About Me</h3>
-            <p>
-              I am a 2nd Year student currently in office as an officer for two organizations. Also inclined with analytics, statistics, as well as data science and similar topics.
-            </p>
-          </div>
+      <section class="main-content">
+        <div class = "main-content-holder">
 
-          <div>
-            <div>
-              <h3>Education</h3>
-              studied two different science high schools, one for Junior High School at 
-              <a href="https://www.facebook.com/profile.php?id=100083844947925" target="_blank">Senator Renato Compañero Cayetano Memorial Science & Technology High School</a> 
-              and another one located at a different city for Senior High School at 
-              <a href="https://www.facebook.com/pcshs.edu" target="_blank">Pasig City Science High School</a>. 
-              With Honors as a grade 12 student as well as a First Year College Student.
-              <br />
-              <b>Achievements</b>
-              <ul>
-                <li>Was the Memberships head for Gaming Genesis</li>
-                <li>Currently the Logistics Head for APC Microsoft Community</li>
-                <li>Current President for Gaming Genesis</li>
-              </ul>
-            </div>
-          </div>
+          <!--About Me-->
 
-          <div>
-            <div>
-              <h3>Course</h3>
-              Currently studying as a Bachelor of Science in Computer Science specializing in Software Systems.
-            </div>
-          </div>
+          <div class="content-container">
+                <div class="content-text">
+                    <h3>About Me</h3>
+                    <p>I am a 2nd Year student currently in office as an officer for two organizations. Also inclined with analytics, statistics, as well as data science and similar topics.</p>
+                </div>
 
-          <div>
-            <div>
-              <h3>IT Experience</h3>
-              Had a 
-              <a href="https://docs.google.com/document/d/1IbS4nP_PUzZyqjC5f1uV26DvfdhpoYD7kBNJlVIQJgo/edit?usp=sharing" target="_blank">Mobile Development Project</a> 
-              that I developed alone for a Group Research Project using Java that works similarly to an Air Tag but has ways of communication to send information for different scenarios such as natural disasters as well as man-made scenarios.
-            </div>
-          </div>
+                    <img src="css/assets/aboutme.png" class="main-content-image" alt="About Me Image">
 
-          <div>
-            <div>
-              <h3>Hobbies & Interests</h3>
-              As a person who doesn't really have anything to do in life, I commonly find myself doing these activities apart from looking at landscapes or "views" like the city lights in the evening.
-              <ul>
-                <li>Reading
-                  <ul>
-                    <li><a href="https://comick.io" target="_blank">Manga/Manhwa</a></li>
-                    <li><a href="https://comick.io" target="_blank">Webtoons</a></li>
-                  </ul>
-                </li>
-                <li>Sleeping</li>
-                <li>Cooking</li>
-                <li>Photography</li>
-                <li>Travelling</li>
-              </ul>
             </div>
-          </div>
+            <div class="content-container">
+            <img class= "main-content-image" src= "css/assets/education.png" >
+        <div class="content-text"><h3>Education</h3>  
+        studied two different science high schools, one for Junior High School at <a href = "https://www.facebook.com/profile.php?id=100083844947925" target="_blank" title = "you can find my school here">Senator Renato Compañero Cayetano Memorial Science & Technology High School</a> and another one located at a different city for Senior High School at <a href="https://www.facebook.com/pcshs.edu" title = "you can find it here!">Pasig City Science High School</a> With Honors as a grade 12 student as well as a First Year College Student.<br>
+    <br>
+    <b>Achievements</b>
+    <ul>
+        <li>Was the Memberships head for Gaming Genesis</li>
+        <li>Currently the Logistics Head for APC Microsoft Community</li>
+        <li>Current President for Gaming Genesis</li>
+    </ul>
+</div>
+</div>
 
-          <div>
-            <div>
-              <h3>Goals In Life / Dream</h3>
-              to be so financially stable that I can spend on a day the average spending power of a person on a year.
-            </div>
-          </div>
+<div class="content-container">
+        <div class="content-text"><h3>Course</h3> 
+        Currently studying as a Bachelor of Science in Computer Science specializing in Software Systems.
+    </div> 
+  
+    <img class = "main-content-image" src = "https://www.apc.edu.ph/wp-content/uploads/2021/09/MicrosoftTeams-image-2-1024x670.jpg" alt= "I Study here!">
+  </div>    
+  <div class="content-container">
+    <img class = "main-content-image" src="https://life360-website-cdn.life360.com/wp-content/uploads/sites/8/2024/03/AU-Homepage-Hero-image.png">
+    <div class="content-text"><h3>IT Experience</h3>
+        Had a <a href = "https://docs.google.com/document/d/1IbS4nP_PUzZyqjC5f1uV26DvfdhpoYD7kBNJlVIQJgo/edit?usp=sharing" target="_blank" title="here is our IMRADC">Mobile Development Project</a> that I developed alone for a Group Research Project using Java that works similarly to an Air Tag but has ways of communication to send information for different scenarios such as natural disasters as well as man-made scenarios
+    </div>
+
+    </div>
+  
+    <div class = "content-container">
+      
+    
+        <div class="content-text"><h3>Hobbies & Interests</h3>
+        As a person who doesn't really have anything to do in life, I commonly find myself doing these activities apart from looking at landscapes or "views" like the city lights in the evening.
+        <br>
+        <li>Reading</li>
+        <ul>
+            <li><a href="comick.io" target="_blank" title = "I read them here">Manga/Manhwa</a></li>
+            <li><a href="comick.io" target="_blank" title  = "I also read them here">Webtoons</a></li>
+        </ul>
+        <li>Sleeping</li>
+        <li>Cooking</li>
+        <li>Photography</li>
+        <li>Travelling</li>
         </div>
-      </section>
+        <img class = "main-content-image" src="https://preview.redd.it/jgl8r3o4eog61.jpg?width=640&crop=smart&auto=webp&s=7740c69eaf34c8766b436bf40c3c6576a513832a">
+      </div>
+      <div class = "content-container">
+        <img class = "main-content-image" src = "https://img.etimg.com/thumb/msid-105174440,width-650,height-488,imgsize-28384,resizemode-75/leodicaprio.jpg">
+      
+        <div class="content-text"><h3>Goals In Life / Dream</h3>
+            to be so financially stable that I can spend on a day the average spending power of a person on a year.  
+        </div>
+        </div>
+      
+
+        </section>
+      
     </main>
-  </div>
+
 </template>
 
 <script>
